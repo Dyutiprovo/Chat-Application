3ace516ad95110c282f58b669bf74d07f0d87ca7
Dyutiprovo <sarkardyutiprovo@gmail.com>
2024-10-25 01:21:47 +0530

    initial commit

---
+++ src/main/java/com/dyuti/websocket/controller/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.dyuti.websocket.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String sender;
+    private String content;
+    private MessageType type; 
+}
---
+++ src/main/java/com/dyuti/websocket/controller/MessageType.java
@@ -0,0 +1,7 @@
+package com.dyuti.websocket.controller;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
